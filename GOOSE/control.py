63ce6ecbd16bb6b25c5ee4c73162043aa35c6217
nvmChris <148861576+nvmChris@users.noreply.github.com>
2026-01-28 08:28:32 +0100

    simple movement(notakeoff)

--- GOOSE/control.py
+++ GOOSE/control.py
@@ -11,13 +11,28 @@ win = pygame.display.set_mode((400, 400))
 pygame.display.set_caption("Tello Control")
 
 def get_keyboard_input():
-    lr, fb, ud, yv = 0, 0, 0, 0
+    lr, fb, ud, yv = 0, 0, 0, 0 # left/right, forward/backward, up/down, yaw velocity
     speed = 100
+    yaw_rate = 90
 
     keys = pygame.key.get_pressed()
 
-    if keys[]
-
+    if keys[pygame.K_w]:
+        fb = speed
+    if keys[pygame.K_s]:
+        fb = -speed
+    if keys[pygame.K_a]:
+        lr = -speed
+    if keys[pygame.K_d]:
+        lr = speed
+    if keys[pygame.K_e]:
+        ud = speed
+    if keys[pygame.K_q]:
+        ud = -speed
+    if keys[pygame.K_RIGHT]:
+        yv = yaw_rate
+    if keys[pygame.K_LEFT]:
+        yv = -yaw_rate
 
     return [lr, fb, ud, yv]
 
