fbb735dec1486a556603185e20d068e41ab562e5
ohSystemmm <leonhardfresacher@gmail.com>
2026-02-04 10:03:46 +0100

    UI Update - Basic UI

--- GOOSE/ui/main.py
+++ GOOSE/ui/main.py
@@ -2,9 +2,19 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.floatlayout import FloatLayout 
 from kivy.lang import Builder
-from kivy.utils import get_color_from_hex
+from kivy.config import Config
+
+Config.set('graphics', 'width', '1920')
+Config.set('graphics', 'height', '1080')
+Config.set('graphics', 'resizable', False)
 
 class TopBar(Widget):
+    pass
+class Stats(Widget):
+    pass
+class Module(Widget):
+    pass
+class Bottom(Widget):
     pass
 
 class DroneApp(App):
